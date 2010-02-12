4be771ba0a3b9581066b5adb35fc676a2a0c73c9
Russ Meyerriecks <datachomper@gmail.com>
2010-02-12 00:02:29 -0600

    Changed the way we read in the buffer. Added a frequency detection print It's currently detecting the fundamental frequency perfectly

--- demo.c
+++ demo.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-//#include <complex.h>
 #include <fftw3.h>
 #include <math.h>
 
 #define FREQ 8000.0	// Hz
 #define WINDOW 50	// ms
-#define N 1024		// # samples
+#define N 512		// # samples
 
 void dump_buffer(void *, size_t);
 void dump_double_buffer(void *, size_t);
 void dump_half_complex(void *buf, size_t size);
+void print_freq(void *buf, size_t size);
 
 int main(int argc, char *argv[]) {
 	FILE *fp;
@@ -43,9 +43,9 @@ int main(int argc, char *argv[]) {
 	}
 
 	// Fill buffer
-	num = fread(buf, 1, flen, fp);
+	num = fread(buf, 2, flen, fp);
 	fclose(fp);
-	//printf("Read %ld bytes\n", (unsigned long)num);
+	printf("Read %ld samples. %1.2f sec of audio\n", (unsigned long)num, num/FREQ);
 	
 	index = buf;
 
@@ -55,20 +55,32 @@ int main(int argc, char *argv[]) {
 	out = (double *) fftw_malloc(sizeof(double)*N);
 	p = fftw_plan_r2r_1d(N, in, out, FFTW_R2HC, FFTW_ESTIMATE);
 
-	int i = 0;
-	for(i; i<N; i++) {
-		in[i] = *index++;
-		//printf("%lf\n", in[i]);
-	}
-	//dump_double_buffer(in, N);
-	fftw_execute(p);
+	int i,j,y;
+	for(i=0, j=0; i<num; i++, j++) {
 
-	// Normalize output
-	for(i=0; i<N; i++) {
-		out[i] = out[i]/(2*N);
+		if((num-i) < N) {
+			printf("Dumping short sample\n");
+			break;
+		}
+
+		in[j] = index[i];
+		//printf("%lf\n", in[j]);
+
+		//dump_double_buffer(in, N);
+		if(j == N) {
+			printf("sample %d\n", i);
+			fftw_execute(p);
+			// Normalize output
+			for(y=0; y<N; y++) {
+				out[y] = out[y]/(2*N);
+			}
+			//dump_double_buffer(out, N);
+			//dump_half_complex(out, N);
+			print_freq(out, N);
+			j=0;
+		}
+	
 	}
-	//dump_double_buffer(out, N);
-	dump_half_complex(out, N);
 		
 	// Cleanup
 	fftw_destroy_plan(p);
@@ -104,4 +116,22 @@ void dump_half_complex(void *buf, size_t size) {
 		//printf("[%dHz]\t\t(%1.5f %1.5f)\t%1.5f\n", (int)(n/(N/FREQ)), index[n], index[size-n], x);
 		printf("%d,%1.2f\n", (int)(n/(N/FREQ)), x);
 	}
+}
+
+void print_freq(void *buf, size_t size) {
+	double *index = buf;
+	double x[size];
+	size_t n = 1;
+	size_t max = 1;
+
+	for(n; n<size/2; n++) {
+		x[n] = sqrt(index[n]*index[n] + index[size - n]*index[size -n]);
+		if(x[n] > x[max]) {
+			max = n;
+		}
+	}
+	printf("Detected frequency %d(%1.2f) \t %d(%1.2f) \t %d(%1.2f)\n",
+				(int)((max-1)/(N/FREQ)), x[max-1],
+				(int)(max/(N/FREQ)), x[max],
+				(int)((max+1)/(N/FREQ)), x[max+1]);
 }
